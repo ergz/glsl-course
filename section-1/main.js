43913e8aa9cab7abde82f8f28d87eb32575b8c5c
ergz <e.emanuel.rodriguez@gmail.com>
2023-07-05 22:54:44 -0700

    initial

---
+++ section-1/main.js
@@ -0,0 +1,73 @@
+import * as THREE from 'three';
+
+class GLSProject {
+    constructor() {
+    }
+
+    async initialize() {
+        this.threejs_ = new THREE.WebGLRenderer();
+        document.body.appendChild(this.threejs_.domElement);
+
+        window.addEventListener('resize', () => {
+            this.onWindowResize_();
+        }, false)
+
+        this.scene_ = new THREE.Scene();
+
+        this.camera_ = new THREE.OrthographicCamera(0, 1, 1, 0, 0.1, 1000);
+        this.camera_.position.set(0, 0, 1);
+
+        await this.setupProject_();
+        this.raf_();
+    }
+
+    async setupProject_() {
+        const vsh = await fetch("./shaders/vertex-shader.glsl");
+        const fsh = await fetch("./shaders/fragment-shader.glsl");
+
+        const material = new THREE.ShaderMaterial({
+            uniforms: {
+                color1: { value: new THREE.Vector4(1,1,0,1)},
+                color2: { value: new THREE.Vector4(0,1,1,1)},
+            },
+            vertexShader: await vsh.text(),
+            fragmentShader: await fsh.text()
+        });
+
+        const colors = [
+            new THREE.Color(0xFFFF00),
+            new THREE.Color(0xFF00FF),
+            new THREE.Color(0x00FFFF),
+            new THREE.Color(0xFF0F0F),
+        ];
+
+        const colorFloats = colors.map(c => c.toArray()).flat();
+
+        const geometry = new THREE.PlaneGeometry(1, 1);
+        geometry.setAttribute(
+            "mydevColors", 
+            new THREE.Float32BufferAttribute(colorFloats, 3));
+
+        const plane = new THREE.Mesh(geometry, material);
+        plane.position.set(0.5, 0.5, 0);
+        this.scene_.add(plane);
+    }
+
+    onWindowResize_() {
+        this.threejs_.setSize(window.innerWidth, window.innerHeight);
+    }
+
+    raf_() {
+        requestAnimationFrame((t) => {
+            this.threejs_.render(this.scene_, this.camera_);
+            this.raf_();
+        });
+    }
+}
+
+let APP_ = null;
+
+window.addEventListener("DOMContentLoaded", async () => {
+    APP_ = new GLSProject();
+    await APP_.initialize();
+})
---
+++ section-1/shaders/vertex-shader.glsl
@@ -0,0 +1,12 @@
+attribute vec3 mydevColors;
+
+varying vec2 vUvs;
+varying vec3 vColor;
+
+void main() {
+    vec4 localPosition = vec4(position, 1.0);
+
+    gl_Position = projectionMatrix * modelViewMatrix * localPosition;
+    vUvs = uv;    
+    vColor = mydevColors;
+}
